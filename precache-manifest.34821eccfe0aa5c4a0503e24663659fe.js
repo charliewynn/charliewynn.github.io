0cd9a896e64e5f81f2101d946790a5e5632dfc57
charlie wynn <github@cwynn.com>
2019-10-23 12:34:00 -0500

    Updates

---
+++ precache-manifest.34821eccfe0aa5c4a0503e24663659fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9f0ca76da881680bd04fb5ca65aa29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2439ade1d03444408ffc",
+    "url": "/static/css/main.eadf2736.chunk.css"
+  },
+  {
+    "revision": "ee049762e6160b98a635",
+    "url": "/static/js/2.dfb4b2f2.chunk.js"
+  },
+  {
+    "revision": "2439ade1d03444408ffc",
+    "url": "/static/js/main.cd6a8cff.chunk.js"
+  },
+  {
+    "revision": "1d9f6d191314a2ba43a7",
+    "url": "/static/js/runtime-main.85f2f90e.js"
+  }
+]);
